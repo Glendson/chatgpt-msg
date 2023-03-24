e8a194a421df1545d294170b242ff39c48a85ba8
Glendson <gztomazetto@gmail.com>
2023-03-24 19:15:28 +0000

    Auth included

---
+++ components/SessionProvider.tsx
@@ -0,0 +1,17 @@
+import { Session } from "next-auth";
+import { SessionProvider as Provider } from "next-auth/react"
+import React from "react";
+
+type Props = {
+    children: React.ReactNode;
+    session: Session | null;
+}
+
+
+export function SessionProvider({children, session }: Props){
+    return (
+        <Provider>
+            {children}
+        </Provider>
+    )
+}
