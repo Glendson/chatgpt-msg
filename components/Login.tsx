3647c139541c03be1252e708195f0c45d3d78729
Glendson <gztomazetto@gmail.com>
2023-03-27 19:30:31 +0100

    bug in client serverside rendering fixed

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import { signIn } from "next-auth/react"
+import Image from "next/image"
+
 function Login() {
     return (
-        <div>Login</div>
+        <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
+            <Image src="https://links.papareact.com/2i6" width={300} height={300} alt="Logo" />
+            <button onClick={() => signIn("google")} className="text-white font-bold text-3xl animate-pulse">
+                Sign in to use ChatGPT
+            </button>
+        </div>
     )
 }
 
-export default Login
+export default Login 
