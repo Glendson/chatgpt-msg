4f5af6d0cf72b85772cf849805d0d95677782844
Glendson <gztomazetto@gmail.com>
2023-03-28 20:29:15 +0100

    chat inside and put in firebase hook

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,19 @@
 'use client'
 
 import { useSession, signOut } from "next-auth/react"
+import { useCollection } from "react-firebase-hooks/firestore"
 import NewChat from "./NewChat"
+import { collection } from "firebase/firestore"
+import { db } from "../firebase"
+import ChatRow from "./ChatRow"
 
 function Sidebar() {
 
-  const {data: session } = useSession()
+  const { data: session } = useSession()
+
+  const [chats, loading, error] = useCollection(
+    session && collection(db, 'users', session.user?.email!, 'chats')
+  )
 
   return (
     <div className="p-2 flex flex-col h-screen">
@@ -18,14 +26,18 @@ function Sidebar() {
             {/* ModelSelection */}
           </div>
 
+          {chats?.docs.map(chat => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
+
         </div>
       </div>
 
       {session && (
-        <img 
-        onClick={() => signOut()}
-        className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50" 
-        src={session.user?.image!} alt="Profile Picture" 
+        <img
+          onClick={() => signOut()}
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+          src={session.user?.image!} alt="Profile Picture"
         />
       )}
 
